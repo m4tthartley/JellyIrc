e936fc3f3b9021ac654e7b6d421e6889dd861c93
Matt Hartley <m4tthartley@gmail.com>
2016-06-27 23:58:17 +0100

    asdind9buwrfoswef

--- main.cc
+++ main.cc
@@ -83,6 +83,9 @@ void *netRead (int socket, gjMemStack *memStack) {
 	if (readSize == -1) {
 		printf("Read error \n\n");
 	}
+	if (readSize <= 0) {
+		return NULL;
+	}
 
 	void *stackMemory = gjPushMemStack(memStack, readSize, true);
 	gjMemcpy(stackMemory, buffer, readSize);
@@ -106,15 +109,57 @@ void netWrite (int socket, const char *data) {
 }
 
 gjMemStack memStack = gjInitMemStack(megabytes(1));
+gjMemStack inputMemStack = gjInitMemStack(kilobytes(1));
+
 
 enum ClientCommandType {
-	COMMAND_NONE,
+	COMMAND_MSG,
 	COMMAND_QUIT,
+	COMMAND_JOIN,
 };
 
 struct ClientCommand {
 	ClientCommandType type;
+	union {
+		struct {
+			char *str;
+		} msg;
+		struct {
+			char *msg;
+		} quit;
+		struct {
+			char *channel;
+		} join;
+	};
 };
+
+char *getWord (char **str) {
+	char *result = NULL;
+	char *s = *str;
+
+	while (*s == ' ' || *s == '\t' || *s == '\r' || *s == '\n') {
+		++s;
+	}
+	if (*s != 0) {
+		result = s;
+	}
+
+	int len = 0;
+	if (result) {
+		while (*s != ' ' && *s != '\t' && *s != '\r' && *s != '\n' && *s != 0) {
+			++len;
+			++s;
+		}
+		// *s = 0;
+		char *mem = gjPushMemStack(&inputMemStack, len+1);
+		gjMemcpy(mem, result, len);
+		mem[len] = 0;
+		result = mem;
+	}
+
+	*str = s;
+	return result;
+}
 
 ClientCommand parseInput (char *input) {
 	ClientCommand command = {};
@@ -122,13 +167,30 @@ ClientCommand parseInput (char *input) {
 	int size = gjStrlen(input);
 	char *s = input;
 	if (*s == '/') {
-		while (*s != ' ' || *s != '\r' || *s != '\n' || (s - input) < size) {
-			*s = 0;
-			printf("command: %s \n\n", input);
-			++s;
+		++s;
+		char *cmdStr = getWord(&s);
+
+		if (gjStrcmp(cmdStr, "quit") == 0) {
+			command.type = COMMAND_QUIT;
+			char *msg = getWord(&s);
+			if (msg) {
+				command.quit.msg = msg;
+			}
 		}
+
+		if (gjStrcmp(cmdStr, "join") == 0) {
+			command.type = COMMAND_JOIN;
+			char *channel = getWord(&s);
+			if (channel) {
+				command.join.channel = channel;
+			}
+		}
+
+		printf("command: %s \n\n", cmdStr);
 	} else {
-		command.type = COMMAND_NONE;
+		printf("msg \n\n");
+		command.type = COMMAND_MSG;
+		command.msg.str = input; // todo: maybe move this into stack memory
 	}
 
 	return command;
@@ -146,8 +208,28 @@ void *clientInputThread (void *arg) {
 		str[size] = 0;
 
 		ClientCommand command = parseInput(str);
+		if (command.type == COMMAND_MSG) {
+			char str[1024];
+			sprintf(str, "%s\r\n", command.msg.str);
+			netWrite(socket, str);
+		}
 		if (command.type == COMMAND_QUIT) {
-			netWrite(socket, "QUIT\r\n");
+			char str[1024];
+			if (command.quit.msg) {
+				sprintf(str, "QUIT :%s\r\n", command.quit.msg);
+			} else {
+				sprintf(str, "QUIT\r\n");
+			}
+			netWrite(socket, str);
+		}
+		if (command.type == COMMAND_JOIN) {
+			char str[1024];
+			if (command.join.channel) {
+				sprintf(str, "JOIN %s\r\n", command.quit.msg);
+				netWrite(socket, str);
+			} else {
+				printf("You must specify a channel name \n\n");
+			}
 		}
 	}
 
@@ -165,11 +247,14 @@ void testConnect () {
 	pthread_create(&thread, NULL, clientInputThread, &socket);
 
 	netWrite(socket, "HELLO\r\n");
-	netWrite(socket, "NICK matt\r\n");
-	netWrite(socket, "USER matt 0 * :Matt H\r\n");
+	netWrite(socket, "NICK matt123\r\n");
+	netWrite(socket, "USER matt123 0 * :Matt H\r\n");
 	
 	while (true) {
 		char *data = (char*)netRead(socket, &memStack);
+		if (!data) {
+			break;
+		}
 		
 		if (gjStrcmp(data, "NOTICE AUTH :*** Found your hostname\r\n") == 0 ||
 			gjStrcmp(data, "NOTICE * :*** Found your hostname\r\n") == 0) {
@@ -196,6 +281,7 @@ void testConnect () {
 		netWrite(socket, str);
 	}*/
 
+	printf("Closing socket... \n\n");
 	close(socket);
 }
 
